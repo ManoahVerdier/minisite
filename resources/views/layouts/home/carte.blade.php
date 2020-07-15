cd04d5b572293bf263baca08d64bb9bf36064b6d
Manoah VERDIER <verdier.developpement@gmail.com>
2020-07-15 08:36:51 +0200

    Maj map

--- resources/views/layouts/home/carte.blade.php
+++ resources/views/layouts/home/carte.blade.php
@@ -1,10 +1,10 @@
 <div class="container-fluid px-0">
     <div class="row mx-0">
         <div class="col-12 text-center p-0 d-none d-md-block">
-            <img class="h-auto w-100" src="{{asset('/img/home/HP_map.webp')}}"/>
+            <img class="h-auto w-100" src="{{asset('/img/home/HP_Map.webp')}}"/>
         </div>
         <div class="col-12 text-center p-0 d-md-none">
-            <img class="h-auto w-100" src="{{asset('/img/home/HP_map_mobile.webp')}}"/>
+            <img class="h-auto w-100" src="{{asset('/img/home/HP_Map_mobile.webp')}}"/>
         </div>
     </div>
 </div>
