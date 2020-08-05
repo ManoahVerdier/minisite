10815be37f784618844fef54b063c3a73750864c
Manoah VERDIER <verdier.developpement@gmail.com>
2020-08-05 15:16:26 +0200

    migrations

---
+++ database/migrations/2020_08_05_131518_create_conseils_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConseilsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conseils', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug', 255)->nullable();
+            $table->string('certification', 255)->nullable();
+            $table->timestamps();
+            $table->text('lien_formation')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conseils');
+    }
+}
