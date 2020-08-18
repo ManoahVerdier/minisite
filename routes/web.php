72b6c41d24be6e1e04c669c1d0ee157331dc0b55
Manoah VERDIER <verdier.developpement@gmail.com>
2020-08-18 09:35:20 +0200

    page qualite

--- routes/web.php
+++ routes/web.php
@@ -83,8 +83,8 @@ Route::get('/nos-clients', 'SiteController@clients')
     ->name('clients');
 Route::get('/nos-partenaires', 'SiteController@partenaires')
     ->name('partenaires');
-Route::get('audit-interne-qualite-externalise', 'SiteController@auditInterne')
-    ->name('audit-interne-qualite-externe');
+Route::get('/audit-interne-qualite-externalise', 'SiteController@auditInterne')
+    ->name('audit-interne-qualite-externalise');
 
 Route::get('/faqs', 'SiteController@faqs')->name('faqs');
 Route::get('/faq/{slug}', 'SiteController@faq')->name('faq_slug');
