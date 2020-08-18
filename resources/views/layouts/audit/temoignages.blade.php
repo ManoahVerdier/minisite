36d13e603a2963f6c9ebcc022f125820b70076fb
Manoah VERDIER <verdier.developpement@gmail.com>
2020-08-18 10:12:32 +0200

    audit

--- resources/views/layouts/audit/temoignages.blade.php
+++ resources/views/layouts/audit/temoignages.blade.php
@@ -1,18 +1,18 @@
 <?php //dd($conseil->temoignages()->get());?>
 <div class="row py-4 d-none d-md-block" id="temoignages"> 
   <div class="col-12">
-    <div class="title w-100 text-center dark-gray mt-5 pt-5 pr-5 mb-5">
+    <div class="title w-100 text-center dark-gray mt-5 pt-5 mb-5">
       <span class="muli-bold dark-gray">Les témoignages<br></span>
       <span class="pretty dark-gray"> de nos clients </span>
     </div>
   </div>
   <div class="col-12 px-0 pb-4">
-    <img src="{{asset('img/conseil/conseil_temoignages.jpg')}}" class="w-100"/>
+    <img src="{{asset('img/audit/conseil_temoignages.jpg')}}" class="w-100"/>
   </div>
   <div class="col-8 offset-2">
-    <p><b>Nous travaillons avec des entreprises, petites et grandes</b></p>
-    <p><b>Nous pouvons aussi vous aider</b></p>
-    <p>
+    <p class="muli-bold text-center"><b>Nous travaillons avec des entreprises, petites et grandes</b></p>
+    <p class="muli-bold text-center"><b>Nous pouvons aussi vous aider</b></p>
+    <p class="muli">
       Nous avons affiné notre expertise en travaillant avec des organisations 
       allant de marques mondialement reconnues à de petites entreprises familiales 
       dans plus de 25 pays. Les jeunes entreprises et les multinationales établies 
