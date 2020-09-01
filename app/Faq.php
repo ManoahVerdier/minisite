1596c85cae9821ab1c0dce982d2529b936479c14
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-01 11:33:46 +0200

    Fil ariane faq

--- app/Faq.php
+++ app/Faq.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Faq extends Model
 {
-    
+    public function faq_categorie()
+    {
+        return $this->belongsTo(\App\FaqCategorie::class);
+    }
 }
