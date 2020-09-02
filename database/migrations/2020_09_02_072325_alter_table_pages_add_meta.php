55c2cbbc53fd788b357e01b4e47cbcf6554eba4c
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-02 09:27:06 +0200

    metas

--- database/migrations/2020_09_02_072325_alter_table_pages_add_meta.php
+++ database/migrations/2020_09_02_072325_alter_table_pages_add_meta.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterTableConseilsAddDemarche extends Migration
+class AlterTablePagesAddMeta extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class AlterTableConseilsAddDemarche extends Migration
      */
     public function up()
     {
-        Schema::table('conseils', function (Blueprint $table) {
-            $table->text('description')->nullable();
+        Schema::table('pages', function (Blueprint $table) {
+            $table->text('description');
+            $table->text('title');
         });
     }
 
@@ -25,8 +26,9 @@ class AlterTableConseilsAddDemarche extends Migration
      */
     public function down()
     {
-        Schema::table('conseils', function (Blueprint $table) {
+        Schema::table('pages', function (Blueprint $table) {
             $table->dropColumn('description');
+            $table->dropColumn('title');
         });
     }
 }
