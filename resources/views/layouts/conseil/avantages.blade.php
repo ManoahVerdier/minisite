3a3b5facd3854906d49f747c580de9796ef37618
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-04 15:56:45 +0200

    debug

--- resources/views/layouts/conseil/avantages.blade.php
+++ resources/views/layouts/conseil/avantages.blade.php
@@ -24,48 +24,53 @@
                 @if($conseil->avantage1!="")
                     {{$conseil->avantage1}}
                 @else
-                    D’un accompagnement personnalisé pour atteindre votre objectif de certification</div>
+                    D’un accompagnement personnalisé pour atteindre votre objectif de certification
                 @endif
+            </div>
             <div class="col-md-1 icon py-4 text-center">✓</div>
         </div>
         <div class="row">
             <div class="col-md-1 number py-4 text-center">02</div>
-            <div class="col-md-10 content border-top py-4 px-0">
+            < class="col-md-10 content border-top py-4 px-0">
                 @if($conseil->avantage2!="")
                     {{$conseil->avantage2}}
                 @else
-                    De la formation et d’échange de bonnes pratiques en conseil {{$conseil->certification}}</div>
+                    De la formation et d’échange de bonnes pratiques en conseil {{$conseil->certification}}
                 @endif
+            </div>
             <div class="col-md-1 icon py-4 text-center">✓</div>
         </div>
         <div class="row ">
             <div class="col-md-1 number py-4 text-center">03</div>
-            <div class="col-md-10 content border-top py-4 px-0">
+            < class="col-md-10 content border-top py-4 px-0">
                 @if($conseil->avantage3!="")
                     {{$conseil->avantage3}}
                 @else
-                    D’outils pratiques pour la mise en œuvre du système de management de la qualité</div>
+                    D’outils pratiques pour la mise en œuvre du système de management de la qualité
                 @endif
+            </div>
             <div class="col-md-1 icon py-4 text-center">✓</div>
         </div>
         <div class="row ">
             <div class="col-md-1 number py-4 text-center">04</div>
-            <div class="col-md-10 content border-top py-4 px-0">
+            < class="col-md-10 content border-top py-4 px-0">
                 @if($conseil->avantage4!="")
                     {{$conseil->avantage4}}
                 @else
-                    D’un suivi tout au long de votre démarche et après l’obtention de la certification {{$conseil->certification}}</div>
+                    D’un suivi tout au long de votre démarche et après l’obtention de la certification {{$conseil->certification}}
                 @endif
+            </div>
             <div class="col-md-1 icon py-4 text-center">✓</div>
         </div>
         <div class="row ">
             <div class="col-md-1 number py-4 text-center">05</div>
-            <div class="col-md-10 content border-top border-bottom py-4 px-0">
+            < class="col-md-10 content border-top border-bottom py-4 px-0">
                 @if($conseil->avantage5!="")
                     {{$conseil->avantage5}}
                 @else
-                    Vous pourrez échanger avec d’autres entreprises qui ont les mêmes problématiques que vous</div>
+                    Vous pourrez échanger avec d’autres entreprises qui ont les mêmes problématiques que vous
                 @endif
+            </div>
             <div class="col-md-1 icon py-4 text-center">✓</div>
         </div>
     </div>
