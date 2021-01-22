4ca8dec8fcf417f29e9ef1eed4a9d0cd6ebb198a
Manoah VERDIER <verdier.developpement@gmail.com>
2021-01-22 11:50:59 +0100

    Contact footer

--- resources/views/layouts/partials/footer/main.blade.php
+++ resources/views/layouts/partials/footer/main.blade.php
@@ -16,7 +16,7 @@
         <div class="row pt-3">
             <div class="col-12 col-md-6 my-5 text-left pl-5 py-2 contact">
                 <p class="ml-4 title pretty mb-0">Besoin d'un conseil ?</p>
-                <a href="" class="ml-4 text-uppercase w-100 d-inline-block blue h4 mt-0 font-weight-bold muli-bold">Contactez-nous !</a>
+                <a href="{{route('contact')}}" class="ml-4 text-uppercase w-100 d-inline-block blue h4 mt-0 font-weight-bold muli-bold">Contactez-nous !</a>
             </div>
             <div class="col-12 col-md-6 my-5 text-left pl-5 pb-2 pt-1">
                 <a href="tel:+33482534478" class="ml-4 w-100 d-inline-block dark-grey h5 mb-0"><b>04 82 53 44 78</b></a>
