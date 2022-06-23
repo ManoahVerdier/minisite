d30f56fd968f5e6403bd15610865a6359b95ef12
Manoah Verdier <verdier.developpement@gmail.com>
2022-06-23 08:19:45 +0200

    TinyMCE cleared

--- public/js/custom_voyager.js
+++ public/js/custom_voyager.js
@@ -1,6 +1,3 @@
 function tinymce_setup_callback(editor) {
-    console.log("M'hello");
-    console.log(editor.settings.toolbar );
     editor.settings.toolbar = editor.settings.toolbar + ' | removeformat'
-    console.log(editor.settings.toolbar );
 }
