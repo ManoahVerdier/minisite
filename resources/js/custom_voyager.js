06770ec9dba4e5c472341559387e62339780ea73
Manoah Verdier <verdier.developpement@gmail.com>
2022-06-23 08:18:58 +0200

    TinyMCE remove format

--- resources/js/custom_voyager.js
+++ resources/js/custom_voyager.js
@@ -1,1 +1,6 @@
-editor.settings.toolbar = editor.settings.toolbar + 'removeformat'
+function tinymce_setup_callback(editor) {
+    console.log("M'hello");
+    console.log(editor.settings.toolbar );
+    editor.settings.toolbar = editor.settings.toolbar + ' | removeformat'
+    console.log(editor.settings.toolbar );
+}
