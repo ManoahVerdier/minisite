9c9f85a0b362f4eb4efc0e904cbcb22c177f4b34
Manoah Verdier <verdier.developpement@gmail.com>
2022-06-23 07:53:15 +0200

    Modif HP et clean

--- resources/views/layouts/home/page.blade.php
+++ resources/views/layouts/home/page.blade.php
@@ -1,5 +1,5 @@
 <div class="container mb-5 muli" id="page-accueil">
-        <div class="row p-3 pl-md-5 pr-md-1 pt-5 justify-content-center h-100">
+        <div class="row p-3 pl-md-5 pr-md-1 pt-5 justify-content h-100">
             <h1 class="mb-5">{{  App\Homepage::first()->title  }}</h1>
             {!! App\Homepage::first()->contenu  !!}
         </div>
